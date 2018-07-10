71ee5938abbfc98d7fafd2f6ed4c5551d2ae35c2
davidcf <martachi07@gmail.com>
2018-07-10 18:37:37 +0200

    añadimos nueva plantilla bootstrat

--- actions/list-actions.php
+++ actions/list-actions.php
@@ -12,53 +12,64 @@ $result = $db->query($query);
 
 ?>
 
+
 <!DOCTYPE html>
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="https://getbootstrap.com/docs/3.3/favicon.ico">
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>N.O.A. - Configurations</title>
+        <link rel='stylesheet' href='../css/bootstrap.min.css'>
+        <link rel='stylesheet' href='../css/style.css'>
+    </head>
+    <body>
 
-    <title>Navbar Template for Bootstrap</title>
+        <div class='wrapper'>
+            <!-- Sidebar Holder -->
+            <nav id='sidebar'>
+                <div class='sidebar-header'>
+                    <img src="img/urban.png" class="img-thumbnail" alt="urbancode">
+                </div>
+                <ul id="menu" class="list-unstyled components">
+                    <li><a href="index.html"><i class="glyphicon glyphicon-th-list"></i>Aplicaciones</a></li>
+                    <li><a href="help.html"><i class="glyphicon glyphicon-cog"></i>Componentes</a></li>
+                </ul>
+            </nav>
+            <!-- Page Content Holder -->
+            <div id='content'>
+                <div class="jumbotron">
+                    <h2 class="display-3">Hello, world!</h2>
+                    <p class="lead"><small>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</small></p>
+                    <button type='button' class='btn btn-primary navbar-btn' >
+                        <span><a  href='#myModal' data-toggle='modal'><strong> Información</strong></a></span>
+                    </button>
 
-    <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/navbar.css" rel="stylesheet">
-  </head>
+                </div>
+                <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+                    <ul  class='nav navbar-nav navbar-right'>
+                        <span id="lastupdate" class="label label-primary"><i class="glyphicon glyphicon-calendar"></i> Aug 28, 2017 12:38:24 PM</span>
+                    </ul>
+                </div>                
+                <h1>Typography</h1>
+                <p>
+                <p>
+                <p>
 
-  <body>
+                <p>
+                <table>
+                    <thead>
+                        <tr><th>Option</th><th>Description</th></tr>
+                    </thead>
+                    <tbody  class="buscar">
+                        <tr><td>data</td><td>path to data files to supply the data that will be passed into templates.</td></tr>
+                        <tr><td>engine</td><td>engine to be used for processing templates. Handlebars is the default.</td></tr>
+                        <tr><td>ext</td><td>extension to be used for dest files.</td></tr>
+                    </tbody>
+                </table>
 
-    <div class="container">
 
-      <!-- Static navbar -->
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="https://getbootstrap.com/docs/3.3/examples/navbar/#">N.O.A.</a>
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-              <li class="active"><a href="https://getbootstrap.com/docs/3.3/examples/navbar/">Default <span class="sr-only">(current)</span></a></li>
-              <li><a href="https://getbootstrap.com/docs/3.3/examples/navbar-static-top/">Static top</a></li>
-              <li><a href="https://getbootstrap.com/docs/3.3/examples/navbar-fixed-top/">Fixed top</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
-
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h2>Accions</h2>
-        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-      </div>
-<table class="table table-striped">
+<table">
   <thead>
     <tr>
       <th scope="col">Mandate 1</th>
@@ -83,20 +94,77 @@ $result = $db->query($query);
   </tbody>
 </table>
 
-      <footer class="mastfoot mt-auto">
-      <BR>
-        <div class="inner">
-          <p>NOA copyright 2018.</p>
+
+                <h2>Headings</h2>
+                <hr />
+                <p>Headings from <code>h1</code> through <code>h6</code> are constructed with a <code>#</code> for each level:</p>
+
+            </div>
+
         </div>
-      </footer>
+        <!--Footer-->
+        <footer class="container-fluid bg-4 text-center">
 
-    </div> <!-- /container -->
+            <!--Copyright-->
+            <div class="footer-copyright">
+                <div class="container-fluid">
+                    <p></p>
+                    <smal> © 2017 Copyright Produccion Central Web / UrbanCode</small>
+                        <p></p>
+                </div>
+            </div>
+            <!--/.Copyright-->
 
+        </footer>
+        <!-- Logout Modal-->
+        <div id='myModal' class='modal fade'>
+            <div class='modal-dialog'>
+                <div class='modal-content'>
+                    <div class='modal-header'>
+                        <button type='button' class='close' data-dismiss='modal'
+                                aria-hidden='true'>&times;</button>
+                        <h4 class='modal-title'>Información</h4>
+                    </div>
+                    <div class='modal-body'>
+                        <address>
+                            <strong>Produccion Central Web / UrbanCode</strong><br>
+                            1355 Market Street, Suite 900<br>
+                            San Francisco, CA 94103<br>
+                            <abbr title="Phone">P:</abbr> (123) 456-7890
+                        </address>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../js/jquery.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-  
-</body></html>
+                        <address>
+                            <strong>Email</strong><br>
+                            <a href="mailto:#">first.last@example.com</a>
+                        </address>
+                    </div>
+                    <div class='modal-footer'>
+                        <button type='button' class='btn btn-default' data-dismiss='modal'>Aceptar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src='../js/jquery-1.12.0.min.js'></script>
+        <script src='../js/bootstrap.min.js'></script>
+        <script type='text/javascript'>
+            for (var i = 0; i < document.links.length; i++) {
+                if (document.links[i].href == document.URL) {
+                    document.links[i].className = 'active';
+                }
+            }
+        </script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                (function ($) {
+                    $('#filtrar').keyup(function () {
+                        var rex = new RegExp($(this).val(), 'i');
+                        $('.buscar tr').hide();
+                        $('.buscar tr').filter(function () {
+                            return rex.test($(this).text());
+                        }).show();
+                    })
+                }(jQuery));
+            });
+        </script>   
+    </body>
+</html>
